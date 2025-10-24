c452a222d65863033af2e21726df4ea84ac9eac5
captaincoro11 <captaincoro444@gmail.com>
2025-10-24 18:59:01 +0530

    added cargo watch

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use axum:: {routing::get,Router};
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new().route("/",get(|| async {"Hello, World!"}));
+    let app = Router::new().route("/hello",get(hello_world));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener,app).await.unwrap();
+}
+
+async fn hello_world()-> String {
+    String::from("Hello, World Papa")
 }
